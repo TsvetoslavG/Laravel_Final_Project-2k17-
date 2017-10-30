406108c57ac1468c2a225a5b630464f5a34e911e
TsvetoslavG <freshymc@abv.bg>
2017-10-30 20:01:52 +0200

    Laravel_files

---
+++ resources/views/master.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>@yield('title')</title>
+		<link type="text/css" href="@yield('css_url')" rel="stylesheet">
+	</head>
+	<body>
+		@section('content')
+		@show
+	</body>
+</html>
